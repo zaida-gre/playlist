96557744247fe5706b27b6aeb6964b558db83c59
zaida-gre <zaida.greer@stu.barren.kyschools.us>
2026-03-13 10:21:57 -0500

    playlist

---
+++ script.js
@@ -0,0 +1,18 @@
+let playlist = ('Liar', 'All They Wanted','I Dont Wanna Love','Cure for Pain');
+let deleted = ""
+alert("Your current playlist contains: "+playlist);
+let addsong = prompt("Would you like to add a song?");
+playlist.push(addsong);
+alert("Your current playlist contains: "+playlist);
+let removed=playlist.pop()
+alert(removed +' was removed from the playlist.');
+let search = prompt("Search by Name")
+if(playlist.includes(search)){
+    alert("Song Found")
+    let i = playlist.indexOf(search)
+    deleted = prompt("Would you like to delete this song?")
+    let z =(deleted=="yes")? playlist.splice(i,1): console.log('Nothing Deleted');
+    ;
+} else {
+    alert("Song not found :(")
+alert("Your current playlist contains: "+playlist);
